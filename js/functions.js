f05f036da004e4c5fbc07c28a0d9b33eaf26aae6
Robert Yoas <robyoas@hotmail.com>
2022-08-30 09:18:47 -0500

    warmups2

---
+++ js/functions.js
@@ -0,0 +1,169 @@
+// "use strict";
+//
+// /**
+//  * TODO:
+//  * Create a function called 'sayHello' that takes a parameter 'name'.
+//  * When called, the function should return a message that says hello to the passed in name.
+//  *
+//  * Example
+//  * > sayHello("codeup") // returns "Hello, codeup!"
+//  */
+//
+// /**"use strict";
+//
+// var myNameIs = 'Bob'; // TODO: Fill in your name here.
+//
+// // TODO:
+// // Create a function called 'sayHello' that takes a parameter 'name'.
+// // When called, the function should log a message that says hello from the passed in name.
+//
+// // TODO: Call the function 'sayHello' passing the variable 'myNameIs' as a parameter.
+//
+// // Don't modify the following line
+// // It generates a random number between 1 and 100 and stores it in random
+// var random = Math.floor((Math.random()*100)+1);
+//
+// // TODO:
+// // Create a function called 'isOdd' that takes a number as a parameter.
+// // The function should use the ternary operator to log a message.
+// // The log should tell the number passed in and whether it is odd or not.
+//
+// // TODO: Call the function 'isOdd' passing the variable 'random' as a parameter.
+//  * TODO:
+//  * Call the function 'sayHello' and pass your name as a string literal argument.
+//  * Store the result of the function call in a variable named 'helloMessage'.
+//  *
+//  * console.log 'helloMessage' to check your work
+//  */
+//
+// /**
+//  * TODO:
+//  * Store your name as a string in a variable named 'myName', and pass that
+//  * variable to the 'sayHello' function. You should see the same output in the
+//  * console.
+//  */
+//
+// // Don't modify the following line, it generates a random number between 1 and 3
+// // and stores it in a variable named random
+// var random = Math.floor((Math.random() * 3) + 1);
+//
+// /**
+//  * TODO:
+//  * Create a function called 'isTwo' that takes a number as a parameter.
+//  * The function should return a boolean value based on whether or not the passed
+//  * number is the number 2.
+//  *
+//  * Example
+//  * > isTwo(1) // returns false
+//  * > isTwo(2) // returns true
+//  * > isTwo(3) // returns false
+//  *
+//  * Call the function 'isTwo' passing the variable 'random' as a argument.
+//  *
+//  * console.log *outside of the function* to check your work (you should see a
+//  * different result everytime you refresh the page if you are using the random
+//  * number)
+//  */
+//
+// /**
+//  * TODO:
+//  * Create a function named 'calculateTip' to calculate a tip on a bill at a
+//  * restaurant. The function should accept a tip percentage and the total of the
+//  * bill, and return the amount to tip
+//  *
+//  * Examples:
+//  * > calculateTip(0.20, 20) // returns 4
+//  * > calculateTip(0.25, 25.50) // returns 6.375
+//  * > calculateTip(0.15, 33.42) // returns 5.013
+//  */
+//
+// /**
+//  * TODO:
+//  * Use prompt and alert in combination with your calculateTip function to
+//  * prompt the user for the bill total and a percentage they would like to tip,
+//  * then display the dollar amount they should tip
+//  */
+//
+// /**
+//  * TODO:
+//  * Create a function named `applyDiscount`. This function should accept a price
+//  * (before a discount is applied), and a discount percentage (a number between 0
+//  * and 1). It should return the result of applying the discount to the original
+//  * price.
+//  *
+//  * Example:
+//  * > var originalPrice = 100;
+//  * > var discountPercent = .2; // 20%
+//  * > applyDiscount(originalPrice, discountPercent) // 80
+//  *
+//  * > applyDiscount(45.99, 0.12) // 40.4712
+//  */
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <title>first_functions</title>
+</head>
+<body>
+<script>
+    function calculateCircumference(radius) {
+    const pi = 3.14159
+    return 2 * pi * radius;
+}
+    function convert(celciusToFarenheit) {
+    let celcius = (* 1.8 + 32 === farenheit){
+    return farenheit};
+}
+    function convertGallonstoLiters {
+    var gallon = ( * 3.785)
+    liters
+    return liters;
+}
+    function calculateKilometersToMiles {
+    let kilometers = ( * 3.785)Miles
+    return kilometers
+}
+
+    function toUpperCase ("asdf") {
+    return ("ASDF")}
+    console.log("ASDF");
+
+    function lowerCase(string) {
+    return string.toLowerCase ()}
+    console.log ();
+
+    function trimString(string){
+    return string.trim()}
+    console.log();
+
+    function stringLength(string){
+    return string.length;
+}
+
+    function length ("ASdf") {
+    return (4)}
+    console.log (4);
+
+    function index (){
+    return index()}
+    console.log();
+
+    function replace (threeStrings) {
+    return (firstString, firstInstance of secondString for charOfThirdString)}
+    console.log ();
+
+    function replace (allInstances secondString){
+    return (thirdString)}
+    console.log (thirdString);
+
+    function subset (string, 2 (numbers){
+    return (numbers)}
+    console.log (numbers);
+
+    function indexOfCharacter(char, string) {
+    return string.indexOf(char);}
+
+    }
+</script>
+</body>
+</html>
---
+++ warmups2.js
@@ -0,0 +1,74 @@
+// function sumArray(array){const ourArray = [18,20,22];
+//     let sum = 0;for(let i = 0; i< ourArray. length;i +=1){
+//     }
+//
+//     function arrayAverage(array)
+    let classes = [
+        {class: "6th grade history", students: 18},
+        {class: "7th grade history", students: 20},
+        {class: "8th grade history", students: 22}
+    ]
+    let numericArray = [8, 10, 20];
+
+    function arraySum(array){
+        let total = 0;
+        for (let i = 0); i < array.length; i++{
+            total += array[i];
+        }
+        return total;
+    }
+const convertToObject = (nameOfClass, numberOfStudents
+)
+console.log(convertToObject)
+const neighborhood1 = {
+    neighborhood: "Luminous Estates",
+    medianhomePrice: 270000,
+    pool: true,
+    tennis: true,
+    crimeRate: "low",
+    schools: [
+        {name: "ES1", rating: 9},
+    ]
+    const neighborhood1 = {
+        neighborhood: "Luminous Estates",
+        medianhomePrice: 270000,
+        pool: true,
+        tennis: true,
+        crimeRate: "low",
+        schools: [
+            {name: "ES1", rating: 9},
+        ]
+const neighborhood1 = {
+        neighborhood: "Luminous Estates",
+    medianhomePrice: 270000,
+    pool: true,
+    tennis: true,
+    crimeRate: "low",
+    schools: [
+        {name: "ES1", rating: 9},
+    ]
+}
+
+function desirableNeighborhood(neighborhoodsObject){
+        let total = 0;
+        for (let i = 0; i < neighborhoodsObject.schools.length; i++)
+            total += neighborhoodsObject.schools[i]rating;
+        return neighborhoodsObject.medianHomePrice < 300000 && neighborhoodsObject.crimeRate === "low"
+}
+
+//beeramid(1500, 2); should === 12
+//beeramid(5000, 3); should === 16
+//level * level * priceOfCan
+
+function beeramid(bonus, price){
+        let totalCost = 0;
+        let levels = 0;
+        while (totalCost += levels * levels * price;
+        totalCost += levels * levels * price;
+        if (totalCost + ((levels +1) ** 2 * price) >
+        bonus){break;}
+        levels++}
+}
+return levels;
+}
+
