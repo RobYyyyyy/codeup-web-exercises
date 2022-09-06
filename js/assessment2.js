0290336ecfa745ac5cc9aabe44b81ad1091dc738
Robert Yoas <robyoas@hotmail.com>
2022-09-06 10:06:54 -0500

    flexbox

--- js/assessment2.js
+++ js/assessment2.js
@@ -1,95 +1,99 @@
 // //define a function named is greater than that takes in two values and returns a true if the first argument is greater than the second argument otherwise it shiould return false.  If either argument is not a number, return false;
 //
-// function isGreaterThan(arg1, arg2){
-//     if(typeof arg1 !== "number" || typeof arg2 !== "number"){
-//     return false;
-// } else if (arg2 > arg1 || arg1 === arg2){
-//     return false;
-//     } else {
-//         return true;
-//     }
-// }
+function isGreaterThan(arg1, arg2){
+    if(typeof arg1 !== "number" || typeof arg2 !== "number"){
+    return false;
+} else if (arg2 > arg1 || arg1 === arg2){
+    return false;
+    } else {
+        return true;
+    }
+}
 // //define a function named isLessThan thst takes in two values and returns a true. If
-// function isLessThan(arg1, arg2){
-//         if(typeof arg1 !== "number" || typeof arg2 !== "number") {
-//                 return false;
-//             } else if (arg2 < arg1 || arg1 === arg2) {
-//                  return false;
-//              } else {
-//          return true;
-//         }
-// }
+function isLessThan(arg1, arg2){
+        if(typeof arg1 !== "number" || typeof arg2 !== "number") {
+                return false;
+            } else if (arg2 < arg1 || arg1 === arg2) {
+                 return false;
+             } else {
+         return true;
+        }
+}
 //     //define a function named isEqualNumber
-// function isEqualNumber(arg1, arg2) {
-//     if (typeof arg1 !== "number" || typeof arg2 !== "number") {
-//         return false;
-//     } else if (arg1 === arg2) {
-//             return true;
-//         }
-//     }
+function isEqualNumber(arg1, arg2) {
+    if (typeof arg1 !== "number" || typeof arg2 !== "number") {
+        return false;
+    } else if (arg1 === arg2) {
+            return true;
+        }
+    }
 // // //define a function named is equal that takes in two values and returns a true if the first argument has the same value and type as the second argument, otherwise it should return false.
-// function isEqual() {
-//     if (arg1 === arg2) {
+///     if (arg1 === arg2) {
 //         return true;
 //     } else {
 //         return false;
 //     }
-// }
+// }/ function isEqual() {
+
 // //     //define a function named isNot
-// function isNot (boolean){
-//     if(boolean === null){
-//     return false;
-//     }
-//     return !boolean;
-//     }
+function isNot (boolean){
+    if(boolean === null){
+    return false;
+    }
+    return !boolean;
+    }
 // //     //named calcAbs
-// function calcAbs(number) {
-//     if (typeof number !== "number" || number === null) {
-//         return 0;}
-//         return Math.abs(number);
-//     }
+function calcAbs(number) {
+    if (typeof number !== "number" || number === null) {
+        return 0;}
+        return Math.abs(number);
+    }
 // //     //calculate cube
-// function calcCube() {
-//     if (typeof number !== "number" || number === null) {
-//         return 0;
-//     }
-//     return number * number * number;
-// }
+function calcCube() {
+    if (typeof number !== "number" || number === null) {
+        return 0;
+    }
+    return number * number * number;
+}
 // //         // type match
-// function isTypeMatch(arg1, arg2) {
-//     if (typeof arg1 === typeof arg2) {
-//         return true;
-//     } else if (arg1 === null || arg2 === null) {
-//         return false;
-//     } else {
-//         return false;
-//     }
-// }
-// //         //define a function named findLongestString that accepts two string arguments  and returns the string
-//     function findLongestString(string1, string2){
-//             if(typeof string1 !== "string" && typeof string2 !== "string") {
-//                 return "";
-//             } else if (typeof string1 === "string" && typeof string2 !== "string")
-//             return string1}
-// } else if (typeof string1 !== "string" && typeof string2 === "string")
-//             return string2}
-// } else if (string1.length > string2.length);
-//                 return string1}
-// } else if (string2.length > string1.length);
-//                 return(string2)}
-//
+function isTypeMatch(arg1, arg2) {
+    if (typeof arg1 === typeof arg2) {
+        return true;
+    } else if (arg1 === null || arg2 === null) {
+        return false;
+    } else {
+        return false;
+    }
+}
+// ////define a function named findLongestString that accepts two string arguments  and returns the string
+    function findLongestString(string1, string2);
+            if(typeof string1 !== "string" && typeof string2 !== "string"); {
+                return ("");
+             else if (typeof string1 === "string" && typeof string2 !== "string") {
+                (return string1);
+            }
+ else if (typeof string1 !== "string" && typeof string2 === "string") {
+            return string2;
+ }
+ else if (string1.length > string2.length); {
+                return string1;
+ }
+ else if (string2.length > string1.length); {
+                return string2;
+ }
+
+
 //           //function named isFactor
-//    function isFactor(number1, number2){
-//     if(typeof number1 !== "number" || typeof number2 !== "number")
-//            return false};
-// { else
-//     if (number2 % number1 === 0) {
-//         return true
-//     }
-//     ;
-//     { else
-//         return false;
-//     }
+   function isFactor(number1, number2){
+    if(typeof number1 !== "number" || typeof number2 !== "number")
+           return false};
+{ else
+    if (number2 % number1 === 0) {
+        return true;
+    }
+    { else
+        return false;
+    }
 // }
 // //    //working with values, variables and data types
 // //        //Using assignment and comparison operators
--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-// "use strict";
+//**"use strict";
 //
 // /**
 //  * TODO:
@@ -9,35 +9,35 @@
 //  * > sayHello("codeup") // returns "Hello, codeup!"
 //  */
 
-function("Hello, codeup!"){
-}
-**"use strict";
+//function("Hello, codeup!"){
+//}
+//**"use strict";
 //
-function myNameIs(Rob) {
-var myNameIs = 'Rob';
-return Rob;
-// TODO: Fill in your name here.
-//
-// // TODO:
-// // Create a function called 'sayHello' that takes a
-// parameter 'name'.
-// // When called, the function should log a message that
-// says hello from the passed in name.
-function sayHello() {
-console.log("Hello");
-}
-// // TODO: Call the function 'sayHello' passing the variable 'myNameIs' as a parameter.
-//
-// // Don't modify the following line
-// // It generates a random number between 1 and 100 and stores it in random
-var random = Math.floor((Math.random()*100)+1);
-//
-// // TODO:
-// // Create a function called 'isOdd' that takes a number as a parameter.
-// // The function should use the ternary operator to log a message.
-// // The log should tell the number passed in and whether it is odd or not.
-function isOdd(num) {
-return number 2 === 1;
+// function myNameIs(Rob) {
+// var myNameIs = 'Rob';
+// return Rob;
+// // TODO: Fill in your name here.
+// //
+// // // TODO:
+// // // Create a function called 'sayHello' that takes a
+// // parameter 'name'.
+// // // When called, the function should log a message that
+// // says hello from the passed in name.
+// function sayHello(name) {
+// console.log("Hello, " + name) + "!");
+// }
+// // // TODO: Call the function 'sayHello' passing the variable 'myNameIs' as a parameter.
+// //
+// // // Don't modify the following line
+// // // It generates a random number between 1 and 100 and stores it in random
+// var random = Math.floor((Math.random()*100)+1);
+// //
+// // // TODO:
+// // // Create a function called 'isOdd' that takes a number as a parameter.
+// // // The function should use the ternary operator to log a message.
+// // // The log should tell the number passed in and whether it is odd or not.
+// function isOdd(num) {
+// return number 2 === 1;
 }
 // // TODO: Call the function 'isOdd' passing the variable 'random' as a parameter.
 //  * TODO:
@@ -46,141 +46,167 @@ return number 2 === 1;
 //  *
 //  * console.log 'helloMessage' to check your work
 //  */
-    var myName ="Rob"
-    comsole.log=
-    function sayHello(myName)
-    var myStr =""
-
-    let helloMessage = sayHello("Rob");
-    console.log(helloMessage);
-// /**
-//  * TODO:
-//  * Store your name as a string in a variable named 'myName', and pass that
-//  * variable to the 'sayHello' function. You should see the same output in the
-//  * console.
-//  */
-    let myName = "Rob";
-    console.log(sayHello(myName));
-// // Don't modify the following line, it generates a random number between 1 and 3
-// // and stores it in a variable named random
- var random = Math.floor((Math.random() * 3) + 1);
+//     var myName ="Rob"
+//     comsole.log=
+//     function sayHello(myName)
+//     var myStr =""
 //
-// /**
-//  * TODO:
+//     let helloMessage = sayHello("Rob");
+//     console.log(helloMessage);
+// // /**
+// //  * TODO:
+// //  * Store your name as a string in a variable named 'myName', and pass that
+// //  * variable to the 'sayHello' function. You should see the same output in the
+// //  * console.
+// //  */
+//     let myName = "Rob";
+//     console.log(sayHello(myName));
+// // // Don't modify the following line, it generates a random number between 1 and 3
+// // // and stores it in a variable named random
+//  var random = Math.floor((Math.random() * 3) + 1);
+// //
+// // /**
+// //  *
 //  * Create a function called 'isTwo' that takes a number as a parameter.
 //  * The function should return a boolean value based on whether or not the passed
 //  * number is the number 2.
+var random = Math.floor((Math.random() * 3) + 1);
 
     function isTwo(number){
         return number === 2;
     }
     console.log(isTwo(random));
-//  * Example
-//  * > isTwo(1) // returns false
-//  * > isTwo(2) // returns true
-//  * > isTwo(3) // returns false
-//  *
-//  * Call the function 'isTwo' passing the variable 'random' as a argument.
-//  *
-//  * console.log *outside of the function* to check your work (you should see a
-//  * different result everytime you refresh the page if you are using the random
-//  * number)
-//  */
+
+//         function  swapBits(x, p1, p2, n)
+//         {
+//             // Move all bits of first set
+//             // to rightmost side
+//             let set1 = (x >> p1) & ((1 << n) - 1);
 //
-// /**
-//  * TODO:
-//  * Create a function named 'calculateTip' to calculate a tip on a bill at a
-//  * restaurant. The function should accept a tip percentage and the total of the
-//  * bill, and return the amount to tip
-//  *
-//  * Examples:
-//  * > calculateTip(0.20, 20) // returns 4
-//  * > calculateTip(0.25, 25.50) // returns 6.375
-//  * > calculateTip(0.15, 33.42) // returns 5.013
-//  */
-    function calculateTip(tipPercentage, billTotal){
-        return billTotal * tipPercentage;
-// /**
-//  * TODO:
-//  * Use prompt and alert in combination with your calculateTip function to
-//  * prompt the user for the bill total and a percentage they would like to tip,
-//  * then display the dollar amount they should tip
-//  */
-        function applyDiscount(priceBeforeDiscount, discountPercentage) {
-            //return priceBeforeDiscount * (1 - discountPercentage);
-            return priceBeforeDiscount - (priceBeforeDiscount * discountPercentage);
-        }
-// /**
-//  * TODO:
-//  * Create a function named `applyDiscount`. This function should accept a price
-//  * (before a discount is applied), and a discount percentage (a number between 0
-//  * and 1). It should return the result of applying the discount to the original
-//  * price.
-//  *
-//  * Example:
-//  * > var originalPrice = 100;
-//  * > var discountPercent = .2; // 20%
-//  * > applyDiscount(originalPrice, discountPercent) // 80
-//  *
-//  * > applyDiscount(45.99, 0.12) // 40.4712
-//  */
-    function calculateCircumference(radius) {
-    const pi = 3.14159
-    return 2 * pi * radius;
-}
-    function convert(celciusToFarenheit) {
-    let celcius = (* 1.8 + 32 === farenheit){
-    return farenheit};
-}
-    function convertGallonstoLiters {
-    var gallon = ( * 3.785)
-    liters
-    return liters;
-}
-    function calculateKilometersToMiles {
-    let kilometers = ( * 3.785)Miles
-    return kilometers
-}
-
-    function toUpperCase ("asdf") {
-    return ("ASDF")}
-    console.log("ASDF");
-
-    function lowerCase(string) {
-    return string.toLowerCase ()}
-    console.log ();
-
-    function trimString(string){
-    return string.trim()}
-    console.log();
-
-    function stringLength(string){
-    return string.length;
-}
-
-    function length ("ASdf") {
-    return (4)}
-    console.log (4);
-
-    function index (){
-    return index()}
-    console.log();
-
-    function replace (threeStrings) {
-    return (firstString, firstInstance of secondString for charOfThirdString)}
-    console.log ();
-
-    function replace (allInstances secondString){
-    return (thirdString)}
-    console.log (thirdString);
-
-    function subset (string, 2 (numbers){
-    return (numbers)}
-    console.log (numbers);
-
-    function indexOfCharacter(char, string) {
-    return string.indexOf(char);}
-
-    }
-</script>
-</body>
+//             // Move all bits of second set
+//             // to rightmost side
+//             let set2 = (x >> p2) & ((1 << n) - 1);
+//
+//             // XOR the two sets
+//             let xor = (set1 ^ set2);
+//
+//             // Put the xor bits back to
+//             // their original positions
+//             xor = (xor << p1) | (xor << p2);
+//
+//             // XOR the 'xor' with the original number
+//             // so that the  two sets are swapped
+//             let result = x ^ xor;
+//
+//             return result;    return number === 2;
+//     }
+//     console.log(isTwo(random));
+// //  * Example
+// //  * > isTwo(1) // returns false
+// //  * > isTwo(2) // returns true
+// //  * > isTwo(3) // returns false
+// //  *
+// //  * Call the function 'isTwo' passing the variable 'random' as a argument.
+// //  *
+// //  * console.log *outside of the function* to check your work (you should see a
+// //  * different result everytime you refresh the page if you are using the random
+// //  * number)
+// //  */
+// //
+// // /**
+// //  * TODO:
+// //  * Create a function named 'calculateTip' to calculate a tip on a bill at a
+// //  * restaurant. The function should accept a tip percentage and the total of the
+// //  * bill, and return the amount to tip
+// //  *
+// //  * Examples:
+// //  * > calculateTip(0.20, 20) // returns 4
+// //  * > calculateTip(0.25, 25.50) // returns 6.375
+// //  * > calculateTip(0.15, 33.42) // returns 5.013
+// //  */
+//     function calculateTip(tipPercentage, billTotal){
+//         return billTotal * tipPercentage;
+// // /**
+// //  * TODO:
+// //  * Use prompt and alert in combination with your calculateTip function to
+// //  * prompt the user for the bill total and a percentage they would like to tip,
+// //  * then display the dollar amount they should tip
+// //  */
+//         function applyDiscount(priceBeforeDiscount, discountPercentage) {
+//             //return priceBeforeDiscount * (1 - discountPercentage);
+//             return priceBeforeDiscount - (priceBeforeDiscount * discountPercentage);
+//         }
+// // /**
+// //  * TODO:
+// //  * Create a function named `applyDiscount`. This function should accept a price
+// //  * (before a discount is applied), and a discount percentage (a number between 0
+// //  * and 1). It should return the result of applying the discount to the original
+// //  * price.
+// //  *
+// //  * Example:
+// //  * > var originalPrice = 100;
+// //  * > var discountPercent = .2; // 20%
+// //  * > applyDiscount(originalPrice, discountPercent) // 80
+// //  *
+// //  * > applyDiscount(45.99, 0.12) // 40.4712
+// //  */
+//     function calculateCircumference(radius) {
+//     const pi = 3.14159
+//     return 2 * pi * radius;
+// }
+//     function convert(celciusToFarenheit) {
+//     let celcius = (* 1.8 + 32 === farenheit){
+//     return farenheit};
+// }
+//     function convertGallonstoLiters {
+//     var gallon = ( * 3.785)
+//     liters
+//     return liters;
+// }
+//     function calculateKilometersToMiles {
+//     let kilometers = ( * 3.785)Miles
+//     return kilometers
+// }
+//
+//     function toUpperCase ("asdf") {
+//     return ("ASDF")}
+//     console.log("ASDF");
+//
+//     function lowerCase(string) {
+//     return string.toLowerCase ()}
+//     console.log ();
+//
+//     function trimString(string){
+//     return string.trim()}
+//     console.log();
+//
+//     function stringLength(string){
+//     return string.length;
+// }
+//
+//     function length ("ASdf") {
+//     return (4)}
+//     console.log (4);
+//
+//     function index (){
+//     return index()}
+//     console.log();
+//
+//     function replace (threeStrings) {
+//     return (firstString, firstInstance of secondString for charOfThirdString)}
+//     console.log ();
+//
+//     function replace (allInstances secondString){
+//     return (thirdString)}
+//     console.log (thirdString);
+//
+//     function subset (string, 2 (numbers){
+//     return (numbers)}
+//     console.log (numbers);
+//
+//     function indexOfCharacter(char, string) {
+//     return string.indexOf(char);}
+//
+//     }
+// </script>
+// </body>
